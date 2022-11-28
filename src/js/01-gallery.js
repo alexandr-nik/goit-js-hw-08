d456434104511ef1386a857a89b0d441e0a39c61
Alexander <107132921+niksemovs@users.noreply.github.com>
2022-11-28 22:13:54 +0200

    change code

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -7,32 +7,22 @@ const galleryEl = document.querySelector('.gallery');
 
 const items = [];
 
-const createElementGallery = function (item) {
-  const itemLi = document.createElement('li');
-  const itemLink = document.createElement('a');
-  const itemImg = document.createElement('img');
-  itemImg.classList = 'gallery__image';
-  itemImg.alt = item.description;
-  itemImg.src = item.preview;
-  itemImg.style.width = '340px';
-  itemLink.classList = 'gallery__item';
-  itemLink.href = item.original;
-  itemLink.append(itemImg);
-  itemLi.append(itemLink);
-  items.push(itemLi);
+const createElementGallery = function () {
+  galleryItems.map(e => {
+    items.push(
+      `<div class="gallery__item">
+  <a href="${e.original}" class="gallery__link">
+  <img class="gallery__image" src="${e.preview}" alt="${e.description}" width="340">
+  </a></div>`
+    );
+  });
 };
+createElementGallery();
+galleryEl.innerHTML = items.join('');
 
-galleryItems.forEach(item => {
-  createElementGallery(item);
-});
-
-galleryEl.append(...items);
-
-const galleryLightbox = new SimpleLightbox('.gallery .gallery__item', {
+const galleryLightbox = new SimpleLightbox('.gallery .gallery__link', {
   captions: true,
   captionDelay: 250,
   captionPosition: 'bottom',
   captionsData: 'alt',
 });
-
-galleryLightbox.on();
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -4,6 +4,10 @@
 // В противном случае поля должны быть пустыми.
 // При сабмите формы очищай хранилище и поля формы, а также выводи объект с полями email, message и текущими их значениями в консоль.
 // Сделай так, чтобы хранилище обновлялось не чаще чем раз в 500 миллисекунд. Для этого добавь в проект и используй библиотеку lodash.throttle.
+// form[refs.inputEl.name] = refs.inputEl.value;
+// form[refs.messageEl.name] = refs.messageEl.value;
+
+
 import throttle from 'lodash.throttle';
 
 function getRefs() {
@@ -15,20 +19,15 @@ function getRefs() {
     ),
   };
 }
+
 const refs = getRefs();
 let form = {};
-function createForm() {
-  form.email = '';
-  form.message = '';
-}
+
 function testLocalStorage() {
-  if (!localStorage.getItem('feedback-form-state')) {
-    createForm();
-    localStorage.setItem('feedback-form-state', JSON.stringify(form));
-  }
-  form = JSON.parse(localStorage.getItem('feedback-form-state'));
+  if (localStorage.getItem('feedback-form-state')) {
+   form = JSON.parse(localStorage.getItem('feedback-form-state'));
   refs.inputEl.value = form.email;
-  refs.messageEl.value = form.message;
+  refs.messageEl.value = form.message;}
 }
 testLocalStorage();
 
@@ -37,15 +36,17 @@ refs.formEl.addEventListener('submit', onSubmitForm);
 function onInputForm(e) {
 form.email = refs.inputEl.value;
 form.message = refs.messageEl.value;
-  localStorage.setItem('feedback-form-state', JSON.stringify(form));
+ localStorage.setItem('feedback-form-state', JSON.stringify(form));
 }
+
 function onSubmitForm(e) {
   e.preventDefault();
   if (form.email === '' || form.message === '') {
     alert('Fill in all the fields');
     return;
   }
+  console.log(form);
   e.currentTarget.reset();
-  createForm();
-  localStorage.setItem('feedback-form-state', JSON.stringify(form));
+  form = {}
+  localStorage.removeItem('feedback-form-state');
 }
